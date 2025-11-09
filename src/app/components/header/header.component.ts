2a58fa42c287e18e90aa9fececaa4cc59326646b
Manuel Jaime <manueljaime0020@gmail.com>
2025-11-09 03:10:25 +0100

    Aplicando responsividade a navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,8 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  LOGO = "LUX"
+  LOGO = 'LUX';
+
+
+  NAV_LINKS = [
+    { id: 1, path: '#', text: 'Home' },
+    { id: 2, path: '#', text: 'About' },
+    { id: 3, path: '#', text: 'Features' },
+    { id: 4, path: '#', text: 'Gallery' },
+    { id: 5, path: '#', text: 'Contact' },
+  ];
+
+  isMenuOpen = false;
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
