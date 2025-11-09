2bda8efe15b4d6b0cd8b71673ab00a9dc3e927e8
Manuel Jaime <manueljaime0020@gmail.com>
2025-11-09 16:56:28 +0100

    Criando about section resposiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { AboutSectionComponent } from "./components/about-section/about-section.component";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { HeroSectionComponent } from './components/hero-section/hero-section.com
     HeaderComponent,
     FooterComponent,
     HeroSectionComponent,
-  ],
+    AboutSectionComponent
+],
   templateUrl: './app.component.html',
   styleUrls: [],
 })
